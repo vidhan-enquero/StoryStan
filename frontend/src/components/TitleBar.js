f417766039d21fc8bcc3fbd572c9cbdee62d8a62
Goyal <850066514@genpact.com>
2023-07-05 00:59:14 +0530

    fixed the code

--- frontend/src/components/TitleBar.js
+++ frontend/src/components/TitleBar.js
@@ -1,37 +1,62 @@
-import react , {useContext} from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
-import { UserContext } from '../context/UserContext';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebase";
+
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
 
 export default function TitleBar(props) {
-  const navigate = useNavigate()
-  const {userInfo , setUserInfo} = useContext(UserContext)
+  const navigate = useNavigate();
+  const { userInfo, setUserInfo } = useContext(UserContext);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             StoryStan
           </Typography>
-          {
-            props.page === "landing" ? <><a>
-            <Button style= {{color: "white"}} onClick={() => navigate("/signup")}>Signup</Button>
-        </a>
-        <a>
-            <Button style= {{color: "white"}} onClick={() => navigate("/login")}>Login</Button>
-        </a></> : <>
-        <a>
-            <Button style= {{color: "white"}} onClick={()=> setUserInfo({isAuthenticated : false})}>Logout</Button>
-        </a>
-        </>
-          }
+          {props.page === "landing" ? (
+            <>
+              <a>
+                <Button
+                  style={{ color: "white" }}
+                  onClick={() => navigate("/signup")}
+                >
+                  Signup
+                </Button>
+              </a>
+              <a>
+                <Button
+                  style={{ color: "white" }}
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </Button>
+              </a>
+            </>
+          ) : (
+            <>
+              <a>
+                <Button
+                  style={{ color: "white" }}
+                  onClick={async () => {
+                    await signOut(auth);
+                    setUserInfo({ isAuthenticated: false });
+                  }}
+                >
+                  Logout
+                </Button>
+              </a>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,27 +1,48 @@
-import { useContext } from "react";
-import { UserContext } from "../context/UserContext";
+import { useContext, useEffect } from "react";
 import Feed from "../components/Feed.js";
-import Sidebar from "../components/SideBar.js"
-import TitleBar from "../components/TitleBar.js"
-import RightBar from "../components/RightBar.js"
+import RightBar from "../components/RightBar.js";
+import Sidebar from "../components/SideBar.js";
+import TitleBar from "../components/TitleBar.js";
+
+import { functions } from "../config/firebase.js";
+import { httpsCallable } from "firebase/functions";
+import { UserContext } from "../context/UserContext.js";
 
 export default function Dashboard() {
-  const { userInfo, setUserInfo } = useContext(UserContext);
+  const { userInfo } = useContext(UserContext);
 
-  const handleLogout = () => {
-    setUserInfo({ isAuthenticated: false });
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      // const verifyUser = httpsCallable(functions, "verifyUser");
+      const response = await fetch(
+        "https://us-central1-storystan-5ff27.cloudfunctions.net/verifyUser",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            token: userInfo.accessToken
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      if (response.status === 200) {
+        alert("Successfully verified the user");
+      } else {
+        alert("Not verified");
+      }
+    };
+    fetchData();
+  });
+
   return (
     <>
-    
-    <TitleBar page="dashboard"/>
-    <div style={{display : "flex" , flexDirection : "row" }}>
-    <Sidebar/>
-      <Feed />
-      <RightBar/>
-    </div>
-      
-
+      <TitleBar page="dashboard" />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Sidebar />
+        <Feed />
+        <RightBar />
+      </div>
     </>
   );
 
